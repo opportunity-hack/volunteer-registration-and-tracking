fa7e8a5ada96272db5c9075d8219209d4c69d2fe
James Quigley <james@quigley.us>
2016-10-01 16:02:12 -0700

    basic form post submission

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var nunjucks = require('nunjucks');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
