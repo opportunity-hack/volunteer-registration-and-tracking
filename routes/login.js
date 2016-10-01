4eceaf86ac1bad475cebd2eab1bb460996c46e57
Rachel Knoche <rkknoche@yahoo.com>
2016-10-01 16:25:21 -0700

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+
+router.get('/', function (req, res) {
+    res.render('index', { user : req.user });
+});
+
+router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
 
