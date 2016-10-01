7893d3ddb974c1b704ad4e69b43588553b042d1c
Rachel Knoche <rkknoche@gmail.com>
2016-10-01 16:13:13 -0700

    JAMES

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+      title: 'Express', 
+      text:{
+          item1:'Hello! (item1)',
+          item2:'Goodbye! (item2)'
+      }, 
+      array:[
+      'arrayitem1',
+      'arrayitem2',
+      'arrayitem3'
+      ]
+  });
 });
 
 module.exports = router;
