04727c222b2d27140caf178ce6c2ba554c0645da
James Quigley <james@quigley.us>
2016-10-01 16:02:12 -0700

    basic form post submission

---
+++ routes/login.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+router.post('/', function (req, res) {
+    res.send('Got a POST request');
+});
+module.exports = router;
