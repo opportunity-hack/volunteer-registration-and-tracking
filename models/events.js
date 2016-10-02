bb2cb325f55da4882546654387ffb5a17d1c3265
Rachel Knoche <rkknoche@gmail.com>
2016-10-01 21:39:17 -0700

    events.js var event

--- models/events.js
+++ models/events.js
@@ -1,1 +1,23 @@
+ar mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
 
+var event = new Schema({
+    eventName: String,
+    volunteers: int,
+    eventDate: Date,
+    location: String,
+    description: String,
+    title: String,
+    minLevel: int,
+    numAttendees: int,
+    training: bool
+
+});
+
+Account.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('Event', Event);
+
+
+};
