ecce715557a1d3ecac51170ea4bc2fd82e3a610d
Rachel Knoche <rkknoche@gmail.com>
2016-10-01 17:36:00 -0700

    login fix

--- routes/login.js
+++ routes/login.js
@@ -2,12 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Login now' });
+  res.render('login', { title: 'Login' });
 });
 
 router.post('/', function (req, res) {
-    res.send('Got a cool POST request');
+    res.send('Got a POST request');
 });
 module.exports = router;
